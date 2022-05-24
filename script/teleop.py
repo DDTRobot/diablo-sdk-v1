174b363b682fd9da024fa5aab05e7a972409f76c
vulcanai <vulcanai@163.com>
2022-05-24 10:54:27 +0800

    first commit

---
+++ script/teleop.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import rospy
+from std_msgs.msg import String
+
+import time
+import sys
+import tty
+import termios
+
+import threading
+
+keyQuee = []
+old_setting = termios.tcgetattr(sys.stdin)
+
+def readchar():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+def getKeyBoard():
+    global keyQuee
+    while True:
+        c = readchar()
+        keyQuee.append(c)
+
+t1 =threading.Thread(target=getKeyBoard)
+t1.setDaemon(True)
+t1.start()
+
+def main():
+    rospy.init_node('dj_teleop')
+    keypub = rospy.Publisher('/DJ_teleop', String, queue_size = 2)
+    while True:
+        if len(keyQuee) > 0:
+            key = keyQuee.pop(0)
+        else:
+            key = " "
+        if key == '`':
+            break
+        keypub.publish(key)
+        time.sleep(0.04)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_setting)
+    print('exit!')
+
+
+if __name__ == '__main__':
+    main()
